2f7ca116486a41d4841d32a3c2f663d0cbb01f21
Kevin-Dobariyaa <kevindobariyaa08@gmail.com>
2025-06-18 18:00:12 +0530

    Backend Setup

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+
+import {connectDB} from "./lib/db.js";
+import authRouter from "./routes/auth.route.js";
+
+ 
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT} 🏃🏃🏃🏃🏃🏃🏃🏃🏃`);
+  connectDB();
+});

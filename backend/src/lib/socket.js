f3d59ba6930aa55d1a54c0c32fe6d46d7835e230
Kevin-Dobariyaa <kevindobariyaa08@gmail.com>
2025-07-23 22:59:10 +0530

    production changes

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -13,6 +13,7 @@ const io = new Server(server, {
   cors: {
     // origin: "http://localhost:5173",
     origin: (NODE_ENV == "developement" ? "http://localhost:5173" : "https://chatty-chat-me.vercel.app"),
+    credentials: true,
   },
 });
 
